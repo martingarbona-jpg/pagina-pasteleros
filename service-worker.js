6a1b2b00a715d49b7aa7f20eb7ab34f932369914
martingarbona-jpg <martingarbona@gmail.com>
2026-06-25 13:59:32 -0300

    ok

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,19 @@
 // VERSION SW 5
 
+const CACHE_NAME = "ospasteleros-v5";
+
 self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    ).then(() => self.clients.claim())
+  );
 });
